e56a0ebbdff9a9886999290397f4c8ea6b3b5a91
SayeedaZeba <31102968+SayeedaZeba@users.noreply.github.com>
2017-08-22 20:07:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,14 @@ console.log('Loaded!');
 //moving image
 
 var img = document.getElementById('dragon');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft= marginLeft + 'px';
+}
 img.onclick = function()
 {
-     img.style.marginLeft = '100px';
+    var interval=setInterval(moveRight,100);
+   
 }
