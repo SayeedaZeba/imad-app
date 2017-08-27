01de6b1d94ce056dbc6f32b5c01892a6207db5ef
SayeedaZeba <31102968+SayeedaZeba@users.noreply.github.com>
2017-08-27 20:34:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,20 @@ button.onclick=function(){
   request.send(null);
   
 };
+
+
+//Submit name
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('sunmit_btn');
+submit.onclick= function(){
+   //Make a request to the server and send the name
+   //Capture a list of names and render it as a list
+    var name=['name1', 'name2', 'name3','name4'];
+    var list='';
+    for( var i=0;i<name.length;i++){
+        list += '<li>' + name[i] + '</li>';
+    }
+ var ul=document.getElementById('namelist');
+ ul.innerHTML=list;
+};
